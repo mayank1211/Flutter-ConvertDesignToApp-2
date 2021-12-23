62991bf86069f03e82ee7814583969fcd6ec034b
Mayank Patel <mayankkumar.patel1@nhs.net>
2021-12-23 14:54:26 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,305 @@
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+// Own modules
+import 'selectedItem.dart';
+
+final List<String> viewListItemNames = ["Sneakers", "Jacket", "Watch"];
+final List<String> viewListIconUrls = [
+  'https://www.vhv.rs/dpng/d/170-1703412_collection-of-free-jordans-drawing-download-on-ui.png',
+  'https://cdn.dribbble.com/users/2744556/screenshots/16993349/media/d490d0324391f9d3f9abe868893b2a13.png',
+  'https://cdn.dribbble.com/users/7135249/screenshots/16894656/media/97b05870189511fae7f8606702642a49.jpg'
+];
+final List<String> viewListSneakersPics = [
+  'sneaker1.png',
+  'sneaker1.png',
+  'sneaker1.png'
+];
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+      // initialRoute: "/",
+      // routes: {
+      //   "/": (_) => MyHomePage(),
+      //   "/item": (BuildContext context) => SelectedItem(),
+      // },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedPage = 0;
+  final _pageOptions = [MainPage(), MainPage(), MainPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(248, 248, 248, 1),
+      appBar: AppBar(
+        elevation: 0,
+        leading: Container(
+          height: 20,
+          width: 20,
+          margin: EdgeInsets.only(left: 15, top: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: IconButton(
+            icon: Icon(
+              Icons.sort,
+              color: Colors.grey,
+              size: 25,
+            ),
+            onPressed: () {
+              print("Pressed");
+            },
+          ),
+        ),
+        title: Text(''),
+        actions: [
+          Container(
+            height: 20,
+            width: 50,
+            margin: EdgeInsets.only(right: 15, top: 10),
+            child: ClipOval(
+              child: Image.network(
+                  'https://source.unsplash.com/50x50/?portrait',
+                  fit: BoxFit.cover),
+            ),
+          ),
+        ],
+        backgroundColor: Color.fromRGBO(248, 248, 248, 1),
+      ),
+      body: _pageOptions[selectedPage],
+      bottomNavigationBar: ConvexAppBar(
+        style: TabStyle.react,
+        items: [
+          TabItem(icon: Icons.home, title: "Home"),
+          TabItem(icon: Icons.search),
+          TabItem(icon: Icons.work),
+          TabItem(icon: Icons.favorite_border)
+        ],
+        initialActiveIndex: 0,
+        onTap: (int i) {
+          setState(
+            () {
+              selectedPage = i;
+              print("index ${i}");
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30, top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Our",
+                style: TextStyle(fontWeight: FontWeight.w400, fontSize: 30)),
+            Text("Products",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+            SizedBox(height: 30),
+            IntrinsicHeight(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      new Flexible(
+                        child: Container(
+                          width: MediaQuery.of(context).size.height * 0.3,
+                          height: 45,
+                          child: new TextField(
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                                filled: true,
+                                contentPadding: EdgeInsets.all(10.0),
+                                fillColor: Colors.grey.shade200,
+                                prefixIcon: Icon(Icons.search),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(
+                                      width: 15.0, color: Colors.grey),
+                                ),
+                                hintText: "Search Products"),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.filter_list),
+                          onPressed: () {
+                            print("Pressed");
+                          },
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            new Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: viewListIconUrls.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.orange.shade600,
+                        width: 2,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(right: 10),
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Image.network(viewListIconUrls[index],
+                            width: 40, height: 30),
+                        SizedBox(width: 5),
+                        Text(
+                          viewListItemNames[index],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              "View All",
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 10),
+            new Row(
+              children: <Widget>[
+                Expanded(
+                  child: SizedBox(
+                    height: 230,
+                    width: 150,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: viewListIconUrls.length,
+                      itemBuilder: (BuildContext ctxt, int index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Future(() {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SelectedItem(
+                                    imageUrl: viewListSneakersPics[index],
+                                  ),
+                                ),
+                              );
+                            });
+                          },
+                          child: Column(
+                            children: [
+                              Row(
+                                children: <Widget>[
+                                  Container(
+                                    height: 230,
+                                    width: 180,
+                                    decoration: BoxDecoration(
+                                        color: Color.fromRGBO(255, 255, 255, 1),
+                                        borderRadius:
+                                            BorderRadius.circular(30)),
+                                    margin: EdgeInsets.only(right: 30),
+                                    child: Column(
+                                      children: [
+                                        Column(
+                                          children: [
+                                            Container(
+                                              alignment: Alignment.topLeft,
+                                              padding: EdgeInsets.only(
+                                                  left: 30, top: 25),
+                                              child:
+                                                  Icon(Icons.favorite_border),
+                                            ),
+                                            Image(
+                                              image: AssetImage(
+                                                  "assets/images/${viewListSneakersPics[index]}"),
+                                              height: 100,
+                                              width: 100,
+                                            ),
+                                            Text(
+                                              "Nike Air Max 200",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Text(
+                                              "Trending Now",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.orange),
+                                            ),
+                                            Text(
+                                              r"$240",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 30),
+            Text(
+              "View All",
+              style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/selectedItem.dart
@@ -0,0 +1,267 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class SelectedItem extends StatefulWidget {
+  SelectedItem({Key? key, this.imageUrl}) : super(key: key);
+  var imageUrl;
+
+  @override
+  State<SelectedItem> createState() => _ItemPageeState();
+}
+
+class _ItemPageeState extends State<SelectedItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color.fromRGBO(248, 248, 248, 1),
+        leading: Container(
+          margin: EdgeInsets.only(left: 15, top: 10),
+          child: IconButton(
+            icon:
+                const Icon(Icons.arrow_back_ios, size: 30, color: Colors.black),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 15, top: 10),
+            child: IconButton(
+              icon: const Icon(Icons.favorite, size: 30, color: Colors.orange),
+              onPressed: () {
+                print("Added to linked");
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Color.fromRGBO(243, 243, 243, 1),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/${widget.imageUrl}",
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: MediaQuery.of(context).size.height * 0.3,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.orange),
+                      ),
+                      child: Image.asset(
+                        "assets/images/${widget.imageUrl}",
+                        fit: BoxFit.cover,
+                      ),
+                      margin: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.orange),
+                      ),
+                      child: Image.asset(
+                        "assets/images/${widget.imageUrl}",
+                        fit: BoxFit.cover,
+                      ),
+                      margin: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.orange),
+                      ),
+                      child: Image.asset(
+                        "assets/images/${widget.imageUrl}",
+                        fit: BoxFit.cover,
+                      ),
+                      margin: EdgeInsets.only(right: 10),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            flex: 2,
+            child: SingleChildScrollView(
+              padding: EdgeInsets.zero,
+              scrollDirection: Axis.vertical,
+              child: Flexible(
+                child: Container(
+                  padding: EdgeInsets.only(left: 30, right: 30),
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.horizontal_rule,
+                          size: 40,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "NIKE AIR MAX 200",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                r"$",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17,
+                                  color: Colors.orange,
+                                ),
+                              ),
+                              Text(
+                                "240",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(right: 15),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(),
+                          IconTheme(
+                            data: IconThemeData(
+                              color: Colors.amber,
+                              size: 15,
+                            ),
+                            child: StarDisplay(value: 4),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        "Available Sizes",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 15),
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          padding: EdgeInsets.zero,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 19,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              child: Row(
+                                children: [
+                                  Text('Entry A'),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Text(
+                        "Color",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 15),
+                        textAlign: TextAlign.left,
+                      ),
+                      Expanded(
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            Text('Entry A'),
+                            Text('Entry A'),
+                            Text('Entry A')
+                          ],
+                        ),
+                      ),
+                      Text(
+                        "Description",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 15),
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                          "Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem IpsumLorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem IpsumLorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem IpsumLorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem IpsumLorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem IpsumLorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem IpsumLorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem IpsumLorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum Lorem Ipsum,  Ipsum."),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: SizedBox(
+        height: 70.0,
+        width: 70.0,
+        child: FittedBox(
+          child: FloatingActionButton(
+            backgroundColor: Colors.orange,
+            foregroundColor: Colors.white,
+            child: Icon(Icons.shopping_basket),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class StarDisplay extends StatelessWidget {
+  final int value;
+  StarDisplay({this.value = 0})
+      : assert(value != null),
+        super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        return Icon(
+          index < value ? Icons.star : Icons.star_border,
+          color: Colors.orange,
+        );
+      }),
+    );
+  }
+}
